f33f545e821cfe53bb5d146a9837f28aaaffa76e
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-02-12 11:54:10 -0300

    brute force palette optimisation

--- README.md
+++ README.md
@@ -1,17 +1,62 @@
+![Original/sprites in OpenMSX](/docs/sprites.png "Original/sprites in OpenMSX")
+
 png2sprites.py
 ==============
 
-Converts png image into MSX2 sprites. Sample image is from the wiki https://www.msx.org/wiki/The_OR_Color
+Converts png image into MSX2 sprites. Resulting sprites will always align perfectly over previous layer. Sample images are from the wiki https://www.msx.org/wiki/The_OR_Color. **png2sprites.py** is based on `png2sprites.py` script tool from reidrac's https://gitlab.com/reidrac/ubox-msx-lib set of libraries.
+
 
 Files
 -----
 
 * `README.md`: this file
-* `png2sprites.py`: conversion script that reads png files in RGB and converts them into MSX2 compatible VRAM sprites;
-* `wit.sh`: simple script that uses data inside `samples` directory as example.
-* `samples/wit.pal`: 15 color triples (RGB) that specifies a MSX2 palette.
+* `png2sprites.py`: conversion script that reads PNG files in RGB and converts them into MSX2 compatible VRAM sprite data.
+* `samples/wit.pal`: 16-colour palette triples (8-bit RGB values) that specify an MSX2 palette. The zeroth colour is used to eliminate PNG background colour.
 * `samples/wit.png`: sample image of Wit from Treasure of Uşas.
 * `samples/WIT.BAS`: simple BASIC code that displays Wit sprite in SCREEN 5.
 * `samples/snake.pal`: 15 color triples (RGB) that specifies a MSX2 palette.
 * `samples/snake.png`: sample image of Snake from Metal Gear 1.
 * `samples/SNAKE.BAS`: simple BASIC code that displays Snake sprite in SCREEN 5.
+
+
+How to use it
+-------------
+
+In addition to the source PNG image, png2sprites.py expects a palette file, which is basically a tuple of 16 RGB triplets.
+
+```
+(255,0,255),(0,0,0),(145,109,0),(109,72,0),(36,36,36),(72,72,72),(109,109,109),(255,255,255),(109,109,109),(0,72,218),(255,0,0),(218,182,145),(182,109,0),(0,145,72),(0,72,36),(0,0,0)
+```
+
+Help message:
+
+```
+usage: png2sprites.py [-h] [--version] [-i ID] [-b] [-a] [-c] [-p PAL_FILE] [-m MIN] image
+
+PNG to MSX2 sprites
+
+positional arguments:
+  image                 image to convert
+
+options:
+  -h, --help            show this help message and exit
+  --version             show program's version number and exit
+  -i ID, --id ID        variable name (default: sprites)
+  -b, --basic           BASIC output (default: C header)
+  -a, --asm             ASM output (default: C header)
+  -c, --colors          include palette colors in C or ASM output
+  -p PAL_FILE, --palette PAL_FILE
+                        set of colors to use from file
+  -m, --minimise        try to minimise palette by brute force
+
+Copyright (C) 2022 Pedro de Medeiros <pedro.medeiros@.gmail.com>
+```
+
+The `--minimise` option will attempt to minimise the number of sprites by or-color replacement after palette permutation and may take several seconds if not minutes. It is an experimental feature and may increase your carbon footprint and return stupid results.
+
+You can refresh sample results with:
+
+```
+./png2sprites.py -i wit -b -p samples/wit.pal samples/wit.png > samples/WIT.BAS
+./png2sprites.py -i snake -b -p samples/snake.pal samples/snake.png > samples/SNAKE.BAS
+```
--- png2sprites.py
+++ png2sprites.py
@@ -24,6 +24,7 @@
 
 import sys
 import contextlib
+import math
 
 from argparse import ArgumentParser
 from itertools import permutations
@@ -35,7 +36,7 @@ DEF_W = 16
 DEF_H = 16
 
 TRANS = (255, 0, 255)
-debug = lambda *x: None
+debug = lambda *x, **y: None
 
 
 def to_hex_list_str(src):
@@ -53,12 +54,8 @@ def to_hex_list_str_asm(src):
     return out
 
 
-def to_hex_list_str_basic(line_num, src):
-    out = ""
-    for i in range(0, len(src), 8):
-        out += "%04d DATA %s\n" % (line_num, ', '.join(["&H%02X" % b for b in src[i:i + 8]]))
-        line_num += 10
-    return line_num, out
+def to_hex_list_str_basic(src):
+    return "DATA %s\n" % ', '.join(["&H%02X" % b for b in src])
 
 
 def decombine_colors(indexes):
@@ -82,6 +79,7 @@ class Sprite:
         self.colors = set()
         self.components = 0
         self.data = []
+        self.pos = None
         for i in range(16):
             self.data.append(dict())
 
@@ -117,68 +115,59 @@ class Sprite:
         return 16 * self.components, 32 * self.components
 
 
-def main():
+def build_lookup_table(palette):
+    lookup = {y: x for x, y in enumerate(palette)} # palette lookup
 
-    parser = ArgumentParser(description="PNG to MSX2 sprites",
-                            epilog="""Copyright (C) 2022 Pedro de Medeiros <pedro.medeiros@.gmail.com>"""
-                            )
+    if len(palette) != 16:
+        print(len(palette))
+        raise OverflowError 
+    for (r, g, b) in palette:
+        if not(int == type(r) == type(g) == type(b)):
+            raise TypeError
 
-    parser.add_argument(
-        "--version", action="version", version="%(prog)s " + __version__)
-    parser.add_argument("-i", "--id", dest="id", default="sprites", type=str,
-                        help="variable name (default: sprites)")
-    parser.add_argument("-b", "--basic", dest="basic", action="store_true",
-                        help="BASIC output (default: C header)")
-    parser.add_argument("-a", "--asm", dest="asm", action="store_true",
-                        help="ASM output (default: C header)")
-    parser.add_argument("-c", "--colors", dest="palette_colors", action="store_true",
-                        help="include palette colors in C or ASM output") # TODO
-    parser.add_argument("-p", "--palette", dest="pal_file", type=str,
-                        help="set of colors to use from file")
+    return lookup
 
-    parser.add_argument("image", help="image to convert")
 
-    args = parser.parse_args()
-
-    try:
-        image = Image.open(args.image)
-    except IOError:
-        parser.error("failed to open the image")
-
-    if image.mode != "RGB":
-        parser.error("not a RGB image (%s detected)" % image.mode)
-
-    with open(args.pal_file, 'r') as pal_file:
-        pal = list(eval(pal_file.read()))
-        lookup = {y:x for x, y in enumerate(pal)} # palette lookup
-        if len(pal) != 16:
-            parser.error("wrong palette size (16 colors expected, got %i)" % len(pal))
-        for (r, g, b) in pal:
-            if not(int == type(r) == type(g) == type(b)):
-                parser.error("wrong palette type")
-
-    (w, h) = image.size
-
-    if w % DEF_W or h % DEF_H:
-        parser.error("%s size is not multiple of sprite size (%s, %s)" %
-                     (args.image, DEF_W, DEF_H))
-
+def get_component_size(image, palette):
     data = image.getdata()
-    sprites = []
-    for i in range(w // DEF_W * h // DEF_H):
-        sprites.append(Sprite())
-
-    debug("sprite map size = %i" % len(sprites))
-    total_bytes = 0
+    colors = {}
+    w, h = image.size
 
     for y in range(0, h, DEF_H):
         for x in range(0, w, DEF_W):
             tile = [data[x + i + ((y + j) * w)]
                     for j in range(DEF_H) for i in range(DEF_W)]
             cols = set([c for c in tile if c is not TRANS])
-            if len([c for c in cols if not c in pal]) > 0:
-                parser.error("Colors used in the image must be present in the palette")
+            if len([c for c in cols if not c in palette]) > 0:
+                raise LookupError
 
+            if not cols: continue
+
+            for j in range(16):
+                for i in range(16):
+                    color = tile[i + j * 16]
+                    if color != (0, 0, 0): colors[color] = True
+
+    return math.ceil(math.log2(len(colors)+0.00001))
+
+
+def build_sprites(image, palette):
+    w, h = image.size
+    data = image.getdata()
+    lookup = build_lookup_table(palette)
+    sprites = []
+
+    for i in range(w // DEF_W * h // DEF_H):
+        sprites.append(Sprite())
+
+    total_bytes = 0
+    max_components = 0
+
+    for y in range(0, h, DEF_H):
+        for x in range(0, w, DEF_W):
+            tile = [data[x + i + ((y + j) * w)]
+                    for j in range(DEF_H) for i in range(DEF_W)]
+            cols = set([c for c in tile if c is not TRANS])
             if not cols: continue
 
             for j in range(16):
@@ -192,6 +181,7 @@ def main():
 
                 for cell, i in ((0, 0), (1, 8)):
                     sprite = sprites[(x // DEF_W) + (y // DEF_H) * (w // DEF_W)]
+                    sprite.pos = x, y
                     byte = [0] * 16
                     p = 7
                     comb = {}
@@ -210,42 +200,131 @@ def main():
                         if byte[color] != 0:
                             sprite.add_line(j, color, cell, byte[color], comb.get(color, False))
 
+            max_components = max(sprite.components, max_components)
             total_bytes += sprite.components * 32 
             total_bytes += sprite.components * 16
 
-    debug(f'components = {sprite.components}')
-    out = []
+    return sprites, max_components, total_bytes
 
-    for sprite in sprites:
+
+def main():
+    parser = ArgumentParser(description="PNG to MSX2 sprites",
+                            epilog="""Copyright (C) 2022 Pedro de Medeiros <pedro.medeiros@.gmail.com>"""
+                            )
+
+    parser.add_argument(
+        "--version", action="version", version="%(prog)s " + __version__)
+    parser.add_argument("-i", "--id", dest="id", default="sprites", type=str,
+                        help="variable name (default: sprites)")
+    parser.add_argument("-b", "--basic", dest="basic", action="store_true",
+                        help="BASIC output (default: C header)")
+    parser.add_argument("-a", "--asm", dest="asm", action="store_true",
+                        help="ASM output (default: C header)")
+    parser.add_argument("-c", "--colors", dest="palette_colors", action="store_true",
+                        help="include palette colors in C or ASM output") # TODO
+    parser.add_argument("-p", "--palette", dest="pal_file", type=str,
+                        help="set of colors to use from file")
+    parser.add_argument("-m", "--minimise", dest="min", type=bool,
+                        help="try to minimise palette by brute force")
+
+    parser.add_argument("image", help="image to convert")
+
+    args = parser.parse_args()
+
+    try:
+        image = Image.open(args.image)
+    except IOError:
+        parser.error("failed to open the image")
+
+    if image.mode != "RGB":
+        parser.error("not a RGB image (%s detected)" % image.mode)
+
+    with open(args.pal_file, 'r') as pal_file:
+        palette = list(eval(pal_file.read()))
+        lookup = {y:x for x, y in enumerate(palette)} # palette lookup
+        if len(palette) != 16:
+            parser.error("wrong palette size (16 colors expected, got %i)" % len(pal))
+        for (r, g, b) in palette:
+            if not(int == type(r) == type(g) == type(b)):
+                parser.error("wrong palette type")
+
+    w, h = image.size
+
+    if w % DEF_W or h % DEF_H:
+        parser.error("%s size is not multiple of sprite size (%s, %s)" %
+                     (args.image, DEF_W, DEF_H))
+
+    try:
+        min_size = get_component_size(image, palette)
+        debug(f'min_size = {min_size}')
+    except LookupError:
+        parser.error("Colors used in the image must be present in the palette")
+
+    min_components = 15
+    best_sprites = None
+    best_pal = None
+    num = 0
+
+    for tmp in permutations(palette[1:]):
+        pal = (palette[0],) + tmp
+        num += 1
+        debug(f"\r{num} pass...", end='')
+        sprites, components, total_bytes = build_sprites(image, pal)
+        if components < min_components:
+            min_components = components
+            debug(f'\nmin_components is now {min_components}')
+            best_sprites, best_pal = sprites, pal
+        if components == min_size:
+            break
+
+    debug(f'components = {min_components}')
+    out = []
+    pos = []
+
+    for sprite in best_sprites:
         for count in range(sprite.components):
             out.append(sprite.get_component(count))
+            pos.append(sprite.pos)
 
     debug(f'total_bytes = {total_bytes}')
+    debug(f'pal = {best_pal}')
 
     if args.basic:
-        line_num = 1000
+        line_num = 100; print('%d SCREEN 5,2' % line_num)
+        line_num += 10; print('%d REM PALETTE' % line_num)
+        for index, color in enumerate(best_pal[1:]):
+            color = round(color[0] / 255 * 7), round(color[1] / 255 * 7), round(color[2] / 255 * 7)
+            line_num += 10; print('%d COLOR=%s' % (line_num, (index + 1,) + color))
+        for i in range(len(out)):
+            line_num += 10; print('%d REM READ %s_COLORS(%d)' % (line_num, args.id.upper(), i))
+            line_num += 10; print('%d A$="":FOR I = 1 TO 16:READ A%%:A$=A$+CHR$(A%%):NEXT:COLOR SPRITE$(%d)=A$' % (line_num, i))
+            line_num += 10; print('%d REM READ %s_PATTERN(%d)' % (line_num, args.id.upper(), i))
+            line_num += 10; print('%d A$="":FOR I = 1 TO 32:READ A%%:A$=A$+CHR$(A%%):NEXT:SPRITE$(%d)=A$' % (line_num, i))
+        line_num += 10; print('%d REM PUT %s SPRITE ON SCREEN' % (line_num, args.id.upper()))
+        for i in range(len(out)):
+            screen_pos = 100 + pos[i][0], 100 + pos[i][1]
+            line_num += 10; print('%d PUT SPRITE %d,%s,,%d' % (line_num, i, screen_pos, i))
+        line_num += 10; print('%d IF INKEY$ = "" GOTO %d' % (line_num, line_num))
+        line_num += 10; print('%d END' % line_num)
+        line_num  = 10**(math.ceil(math.log10(line_num)+0.00001))
         print("%d REM %s_TOTAL = %d" % (line_num, args.id.upper(), total_bytes))
-        line_num += 10
-
-        color_out = ""; pattern_out = ""
-        for i, count in enumerate(range(len(out))):
-            print("%d REM %s_COLORS(%d)" % (line_num, args.id.upper(), i))
-            line_num, data = to_hex_list_str_basic(line_num + 10, out[count]['colors'])
-            print(data, end='')
-            print("%d REM %s_PATTERN(%d)" % (line_num, args.id.upper(), i))
-            line_num, data = to_hex_list_str_basic(line_num + 10, out[count]['patterns'])
-            print(data, end='')
+        for i in range(len(out)):
+            line_num += 10; print("%d REM %s_COLORS(%d)" % (line_num, args.id.upper(), i))
+            line_num += 10; print("%d %s" % (line_num, to_hex_list_str_basic(out[i]['colors'])), end='')
+            line_num += 10; print("%d REM %s_PATTERN(%d)" % (line_num, args.id.upper(), i))
+            line_num += 10; print("%d %s" % (line_num, to_hex_list_str_basic(out[i]['patterns'])), end='')
+        print()
 
     elif args.asm:
         print("%s_TOTAL = %d\n" % (args.id.upper(), total_bytes))
         print("%s:\n" % args.id)
 
         color_out = ""; pattern_out = ""
-        for i, count in enumerate(range(len(out))):
+        for i in range(len(out)):
             print("%s_color%d:" % (args.id, i))
-            print(to_hex_list_str_asm(out[count]['colors']))
+            print(to_hex_list_str_asm(out[i]['colors']))
             print("%s_pattern%d:" % (args.id, i))
-            print(to_hex_list_str_asm(out[count]['patterns']))
+            print(to_hex_list_str_asm(out[i]['patterns']))
     else:
         print("#ifndef _%s_H" % args.id.upper())
         print("#define _%s_H\n" % args.id.upper())
